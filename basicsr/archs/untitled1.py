7c4380404e471128c3f9c243e588ae2cbb631f50
sjnkup <13306999209@163.com>
2023-06-09 18:01:27 +0800

    提交文件

---
+++ basicsr/archs/untitled1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  7 11:27:45 2023
+
+@author: 75221
+"""
+def add(a,b):
+    x = a+b
+    return x
+
+if __name__ == '__main__':
+    x = add(1,2)
+    print("x:",x)
---
+++ basicsr/version.py
@@ -0,0 +1,5 @@
+# GENERATED VERSION FILE
+# TIME: Sun May  7 10:33:11 2023
+__version__ = '1.4.2'
+__gitsha__ = '033cd68'
+version_info = (1, 4, 2)
